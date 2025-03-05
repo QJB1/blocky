371a7f7cf8b7da83cf08ab824195b62fb8dde02a
QJB1 <raychu23001@gmail.com>
2025-03-04 23:28:40 -0600

    Fixed bug causing ArrayIndexOutOfBoundsException

--- src/main/java/com/gamewerks/blocky/engine/Board.java
+++ src/main/java/com/gamewerks/blocky/engine/Board.java
@@ -14,7 +14,7 @@ public class Board {
     }
     
     public boolean isValidPosition(int row, int col) {
-        return row >= 0 && row <= well.length && col >= 0 && col < well[0].length; // it was row <= well.length before
+        return row >= 0 && row < well.length && col >= 0 && col < well[0].length; // it was row <= well.length before
     }
     
     public boolean collides(Piece p) {
